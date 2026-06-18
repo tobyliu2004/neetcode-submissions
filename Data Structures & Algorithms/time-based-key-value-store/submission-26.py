94c9601910f5f67280a0554776f720e4f75b5e10
tobyliu2004 <111081376+tobyliu2004@users.noreply.github.com>
2026-06-18 12:44:30 -0700

    Add: time-based-key-value-store - submission-26

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-26.py
@@ -0,0 +1,28 @@
+class TimeMap:
+
+    def __init__(self):
+        self.case = {}
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.case or not self.case[key]:
+            self.case[key] = []
+        self.case[key].append([timestamp, value])
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.case or not self.case[key]:
+            return ""
+        temp = self.case[key]
+        l = 0
+        r = len(temp)-1
+        while l <= r:
+            mid = (l+r)//2
+            if temp[mid][0] == timestamp:
+                return temp[mid][1]
+            elif temp[mid][0] > timestamp:
+                r = mid - 1
+            else:
+                l = mid + 1
+        if r >= 0:
+            return temp[r][1]
+        else:
+            return ""

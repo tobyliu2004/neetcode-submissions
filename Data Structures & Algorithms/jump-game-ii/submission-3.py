95995710ca0b2283e15c79005f73240001b24fc4
tobyliu2004 <111081376+tobyliu2004@users.noreply.github.com>
2026-07-23 18:22:04 -0700

    Add: jump-game-ii - submission-3

---
+++ Data Structures & Algorithms/jump-game-ii/submission-3.py
@@ -0,0 +1,12 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        l,r = 0,0
+        res = 0
+        track = 0
+        while r < len(nums)-1:
+            for i in range(l,r+1):
+                track = max(track, nums[i]+i)
+            l = r+1
+            r = track
+            res += 1
+        return res

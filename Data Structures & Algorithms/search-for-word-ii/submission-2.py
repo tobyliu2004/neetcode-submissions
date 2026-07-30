97bad0d0b33755355179a5e0f697cef21dadfe9f
tobyliu2004 <111081376+tobyliu2004@users.noreply.github.com>
2026-07-29 17:32:30 -0700

    Add: search-for-word-ii - submission-2

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-2.py
@@ -0,0 +1,39 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.end = False
+    
+    def addWord(self, word):
+        cur = self
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.end = True
+
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        root = TrieNode()
+        for w in words:
+            root.addWord(w)
+        
+        rows = len(board)
+        cols = len(board[0])
+        visit, res = set(), set()
+        def dfs(r,c,node,word):
+            if r not in range(rows) or c not in range(cols) or (r,c) in visit or board[r][c] not in node.children:
+                return
+            visit.add((r,c))
+            node = node.children[board[r][c]]
+            word += board[r][c]
+            if node.end == True:
+                res.add(word)
+            dfs(r+1,c,node,word)
+            dfs(r-1,c,node,word)
+            dfs(r,c+1,node,word)
+            dfs(r,c-1,node,word)
+            visit.remove((r,c))
+        for r in range(rows):
+            for c in range(cols):
+                dfs(r,c,root,"")
+        return list(res)

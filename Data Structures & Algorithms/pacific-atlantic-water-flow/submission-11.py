9c296aa656388c692ff4aa48ed7d71e01379f27a
tobyliu2004 <111081376+tobyliu2004@users.noreply.github.com>
2026-08-01 11:00:23 -0500

    Add: pacific-atlantic-water-flow - submission-11

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-11.py
@@ -0,0 +1,26 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        pac = set()
+        atl = set()
+        rows = len(heights)
+        cols = len(heights[0])
+        def dfs(r,c,ocean,prev):
+            if r in range(rows) and c in range(cols) and (r,c) not in ocean and prev <= heights[r][c]:
+                ocean.add((r,c))
+                dfs(r+1,c,ocean,heights[r][c])
+                dfs(r-1,c,ocean,heights[r][c])
+                dfs(r,c+1,ocean,heights[r][c])
+                dfs(r,c-1,ocean,heights[r][c])
+            
+        for r in range(rows):
+            dfs(r,0,pac,0)
+            dfs(r,cols-1, atl,0)
+        for c in range(cols):
+            dfs(0,c,pac,0)
+            dfs(rows-1,c,atl,0)
+        res = []
+        for r in range(rows):
+            for c in range(cols):
+                if (r,c) in atl and (r,c) in pac:
+                    res.append([r,c])
+        return res
